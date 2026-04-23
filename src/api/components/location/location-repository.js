bd470d0db303d0ae5cd9e28aa513b980277c3d91
veny Laurents <venylaurents09@gmail.com>
2026-04-24 02:04:46 +0700

    Update location-repository.js

--- src/api/components/location/location-repository.js
+++ src/api/components/location/location-repository.js
@@ -1,1 +1,19 @@
 
+module.exports = (mongoose) => {
+  const schema = new mongoose.Schema({
+    pickup_location: {
+      type: String,
+      required: true,
+    },
+    destination_location: {
+      type: String,
+      required: true,
+    },
+    created_at: {
+      type: Date,
+      default: Date.now,
+    },
+  });
+
+  return mongoose.model('Location', schema);
+};
